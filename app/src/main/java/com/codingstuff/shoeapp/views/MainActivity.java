25935b0d05bb960b9957b1f270e536710a76474e
Shivani <shivani.singh@ikure.in>
2023-12-24 11:37:38 +0530

    initial commit

---
+++ app/src/main/java/com/codingstuff/shoeapp/views/MainActivity.java
@@ -0,0 +1,303 @@
+package com.codingstuff.shoeapp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codingstuff.shoeapp.R;
+import com.codingstuff.shoeapp.utils.adapter.ShoeItemAdapter;
+import com.codingstuff.shoeapp.utils.model.ShoeCart;
+import com.codingstuff.shoeapp.utils.model.ShoeItem;
+import com.codingstuff.shoeapp.viewmodel.CartViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ShoeItemAdapter.ShoeClickedListeners {
+    public BottomNavigationView bottomNavigationView;
+    RecyclerView recyclerView;
+    private List<ShoeItem> shoeItemList;
+    ShoeItemAdapter adapter;
+    private CartViewModel viewModel;
+    private List<ShoeCart> shoeCartList;
+    private CoordinatorLayout coordinatorLayout;
+    private ImageView cartImageView;
+    private SearchView searchView;
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.search_view);
+        textView = findViewById(R.id.textView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                textView.setVisibility(newText.isEmpty() ? View.VISIBLE : View.GONE);
+                filterList(newText);
+                return true;
+            }
+
+            private void filterList(String text) {
+                List<ShoeItem> filteredList = new ArrayList<>();
+                for (ShoeItem item : shoeItemList) {
+                    if (item.getShoeBrandName().toLowerCase().contains(text.toLowerCase())) {
+                        filteredList.add(item);
+                    }
+
+                }
+                if (filteredList.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "No data found", Toast.LENGTH_SHORT).show();
+                } else {
+                    adapter.setFilteredList(filteredList);
+                }
+            }
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.person:
+                        return true;
+
+                }
+                return false;
+            }
+
+        });
+
+        initializeVariables();
+        setUpList();
+
+
+        adapter.setShoeItemList(shoeItemList);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        viewModel.getAllCartItems().observe(this, new Observer<List<ShoeCart>>() {
+            @Override
+            public void onChanged(List<ShoeCart> shoeCarts) {
+                shoeCartList.addAll(shoeCarts);
+            }
+        });
+    }
+
+    public void setUpList() {
+        shoeItemList.add(new ShoeItem("Nike Revolution", "Nike", R.drawable.nike_revolution_road, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Revolution", "Nike", R.drawable.nike_revolution_road, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Revolution", "Nike", R.drawable.nike_revolution_road, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Revolution", "Nike", R.drawable.nike_revolution_road, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Revolution", "Nike", R.drawable.nike_revolution_road, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+        shoeItemList.add(new ShoeItem("Nike Flex Run 2021", "NIKE", R.drawable.flex_run_road_running, 20));
+        shoeItemList.add(new ShoeItem("Court Zoom Vapor", "NIKE", R.drawable.nikecourt_zoom_vapor_cage, 18));
+        shoeItemList.add(new ShoeItem("EQ21 Run COLD.RDY", "ADIDAS", R.drawable.adidas_eq_run, 16.5));
+        shoeItemList.add(new ShoeItem("Adidas Ozelia", "ADIDAS", R.drawable.adidas_ozelia_shoes_grey, 20));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 22));
+        shoeItemList.add(new ShoeItem("Adidas Questar", "ADIDAS", R.drawable.adidas_questar_shoes, 12));
+        shoeItemList.add(new ShoeItem("Adidas Ultraboost", "ADIDAS", R.drawable.adidas_ultraboost, 15));
+
+
+    }
+
+    public void initializeVariables() {
+
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        shoeCartList = new ArrayList<>();
+        viewModel = new ViewModelProvider(this).get(CartViewModel.class);
+        shoeItemList = new ArrayList<>();
+        recyclerView = findViewById(R.id.mainRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter = new ShoeItemAdapter(this);
+    }
+
+
+    @Override
+    public void onCardClicked(ShoeItem shoe) {
+        Intent intent = new Intent(MainActivity.this, DetailedActivity.class);
+        intent.putExtra("shoeItem", shoe);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onAddToCartBtnClicked(ShoeItem shoeItem) {
+        ShoeCart shoeCart = new ShoeCart();
+        shoeCart.setShoeName(shoeItem.getShoeName());
+        shoeCart.setShoeBrandName(shoeItem.getShoeBrandName());
+        shoeCart.setShoePrice(shoeItem.getShoePrice());
+        shoeCart.setShoeImage(shoeItem.getShoeImage());
+
+        final int[] quantity = {1};
+        final int[] id = new int[1];
+
+        if (!shoeCartList.isEmpty()) {
+            for (int i = 0; i < shoeCartList.size(); i++) {
+                if (shoeCart.getShoeName().equals(shoeCartList.get(i).getShoeName())) {
+                    quantity[0] = shoeCartList.get(i).getQuantity();
+                    quantity[0]++;
+                    id[0] = shoeCartList.get(i).getId();
+                }
+            }
+        }
+
+        Log.d("TAG", "onAddToCartBtnClicked: " + quantity[0]);
+
+        if (quantity[0] == 1) {
+            shoeCart.setQuantity(quantity[0]);
+            shoeCart.setTotalItemPrice(quantity[0] * shoeCart.getShoePrice());
+            viewModel.insertCartItem(shoeCart);
+        } else {
+            viewModel.updateQuantity(id[0], quantity[0]);
+            viewModel.updatePrice(id[0], quantity[0] * shoeCart.getShoePrice());
+        }
+
+        makeSnackBar("Item Added To Cart");
+    }
+
+    public void makeSnackBar(String msg) {
+        Snackbar.make(coordinatorLayout, msg, Snackbar.LENGTH_SHORT)
+                .setAction("Go to Cart", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(MainActivity.this, CartActivity.class));
+                    }
+                }).show();
+    }
+
+
+
+}
+
---
+++ app/src/test/java/com/codingstuff/shoeapp/views/MainActivityTest.java
@@ -0,0 +1,123 @@
+package com.codingstuff.shoeapp.views;
+
+import android.content.Intent;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.codingstuff.shoeapp.R;
+import com.codingstuff.shoeapp.views.CartActivity;
+import com.codingstuff.shoeapp.views.DetailedActivity;
+import com.codingstuff.shoeapp.utils.adapter.ShoeItemAdapter;
+import com.codingstuff.shoeapp.utils.model.ShoeCart;
+import com.codingstuff.shoeapp.utils.model.ShoeItem;
+import com.codingstuff.shoeapp.viewmodel.CartViewModel;
+import com.codingstuff.shoeapp.views.MainActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainActivityTest {
+
+    @Mock
+    private BottomNavigationView bottomNavigationView;
+
+    @Mock
+    private RecyclerView recyclerView;
+
+    @Mock
+    private ShoeItemAdapter adapter;
+
+    @Mock
+    private CartViewModel viewModel;
+
+    @Mock
+    private Snackbar snackbar;
+
+    @Mock
+    private CoordinatorLayout coordinatorLayout;
+
+    @InjectMocks
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mainActivity = spy(new MainActivity());
+        doNothing().when(mainActivity).initializeVariables();
+        doNothing().when(mainActivity).setUpList();
+        doNothing().when(mainActivity).makeSnackBar(anyString());
+        doNothing().when(mainActivity).setContentView(anyInt());
+        doNothing().when(mainActivity).onCreate(any());
+        when(mainActivity.findViewById(eq(R.id.bottomNavigationView))).thenReturn(bottomNavigationView);
+        when(mainActivity.findViewById(eq(R.id.mainRecyclerView))).thenReturn(recyclerView);
+        when(mainActivity.findViewById(eq(R.id.coordinatorLayout))).thenReturn(coordinatorLayout);
+        when(mainActivity.findViewById(eq(R.id.search_view))).thenReturn(mock(SearchView.class));
+        when(mainActivity.findViewById(eq(R.id.textView))).thenReturn(mock(TextView.class));
+    }
+
+    @Test
+    public void testOnCreate() {
+        mainActivity.onCreate(null);
+        verify(mainActivity).initializeVariables();
+        verify(mainActivity).setUpList();
+        verify(mainActivity).setContentView(anyInt());
+        verify(bottomNavigationView).setSelectedItemId(R.id.person);
+        verify(bottomNavigationView).setOnNavigationItemSelectedListener(any());
+        verify(adapter).setShoeItemList(anyList());
+        verify(recyclerView).setAdapter(adapter);
+    }
+
+    @Test
+    public void testOnResume() {
+        List<ShoeCart> shoeCartList = new ArrayList<>();
+        when(viewModel.getAllCartItems()).thenReturn(mockLiveData(shoeCartList));
+        mainActivity.onResume();
+        verify(viewModel).getAllCartItems();
+    }
+
+    @Test
+    public void testOnCardClicked() {
+        ShoeItem shoeItem = mock(ShoeItem.class);
+        Intent intent = mock(Intent.class);
+
+        mainActivity.onCardClicked(shoeItem);
+
+        verify(mainActivity).startActivity(intent);
+    }
+
+    @Test
+    public void testOnAddToCartBtnClicked() {
+        ShoeItem shoeItem = mock(ShoeItem.class);
+        ShoeCart shoeCart = mock(ShoeCart.class);
+        when(shoeItem.getShoeName()).thenReturn("Test Shoe");
+        when(shoeItem.getShoeBrandName()).thenReturn("Test Brand");
+        when(shoeItem.getShoePrice()).thenReturn(20.0);
+        when(shoeItem.getShoeImage()).thenReturn(123);
+        mainActivity.onAddToCartBtnClicked(shoeItem);
+        verify(mainActivity).makeSnackBar("Item Added To Cart");
+        verify(viewModel).insertCartItem(any());
+    }
+
+    private <T> LiveData<T> mockLiveData(T data) {
+        LiveData<T> liveData = mock(LiveData.class);
+        when(liveData.getValue()).thenReturn(data);
+        return liveData;
+    }
+}
